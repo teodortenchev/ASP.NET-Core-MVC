9735196274d35730448fa622991ee9329db7ffe0
Teodor Tenchev <teodortenchev@gmail.com>
2019-09-08 20:51:12 +0400

    Revert "ok"
    This reverts commit 8a9f654f04558bdd8bd1e2399f6d0884786afbde.

--- Eventures/Eventures/Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ Eventures/Eventures/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -1,10 +1,13 @@
-﻿using Eventures.Domain;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
-using System.Threading.Tasks;
+using Eventures.Domain;
 
 namespace Panda.App.Areas.Identity.Pages.Account
 {
