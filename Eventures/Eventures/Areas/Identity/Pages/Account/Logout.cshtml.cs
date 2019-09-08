fb4098aadf15382d3198bce2d542a5565878e605
Teodor Tenchev <teodortenchev@gmail.com>
2019-09-08 21:21:05 +0400

    .

--- Eventures/Eventures/Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ Eventures/Eventures/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -7,16 +7,17 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using Eventures.Domain;
 
 namespace Eventures.Areas.Identity.Pages.Account
 {
     [AllowAnonymous]
     public class LogoutModel : PageModel
     {
-        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly SignInManager<User> _signInManager;
         private readonly ILogger<LogoutModel> _logger;
 
-        public LogoutModel(SignInManager<IdentityUser> signInManager, ILogger<LogoutModel> logger)
+        public LogoutModel(SignInManager<User> signInManager, ILogger<LogoutModel> logger)
         {
             _signInManager = signInManager;
             _logger = logger;
@@ -27,6 +28,8 @@ namespace Eventures.Areas.Identity.Pages.Account
         {
             await _signInManager.SignOutAsync();
             _logger.LogInformation("User logged out.");
+            returnUrl = "/";
+
             if (returnUrl != null)
             {
                 return LocalRedirect(returnUrl);
