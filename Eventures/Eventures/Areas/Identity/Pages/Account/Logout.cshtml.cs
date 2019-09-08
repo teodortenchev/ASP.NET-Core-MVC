2074e12657c343bdb034188f5ed95a0caf8e753c
Teodor Tenchev <teodortenchev@gmail.com>
2019-09-08 20:45:58 +0400

    Changed logout to work on get request.
    Will not be using the logout landing page.

--- Eventures/Eventures/Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ Eventures/Eventures/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -7,29 +7,29 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using Eventures.Domain;
 
-namespace Eventures.Areas.Identity.Pages.Account
+namespace Panda.App.Areas.Identity.Pages.Account
 {
     [AllowAnonymous]
     public class LogoutModel : PageModel
     {
-        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly SignInManager<User> _signInManager;
         private readonly ILogger<LogoutModel> _logger;
 
-        public LogoutModel(SignInManager<IdentityUser> signInManager, ILogger<LogoutModel> logger)
+        public LogoutModel(SignInManager<User> signInManager, ILogger<LogoutModel> logger)
         {
             _signInManager = signInManager;
             _logger = logger;
         }
 
-        public void OnGet()
-        {
-        }
-
-        public async Task<IActionResult> OnPost(string returnUrl = null)
+        public async Task<IActionResult> OnGet(string returnUrl = null)
         {
             await _signInManager.SignOutAsync();
             _logger.LogInformation("User logged out.");
+
+            returnUrl = "/Home/Index";
+
             if (returnUrl != null)
             {
                 return LocalRedirect(returnUrl);
