520f6b25bff90295c2183d74630bd4bda26c4bd4
Teodor Tenchev <teodortenchev@gmail.com>
2019-09-08 20:52:55 +0400

    Fixed logout issue

--- Eventures/Eventures/Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ Eventures/Eventures/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -22,11 +22,8 @@ namespace Eventures.Areas.Identity.Pages.Account
             _logger = logger;
         }
 
-        public void OnGet()
-        {
-        }
-
-        public async Task<IActionResult> OnPost(string returnUrl = null)
+      
+        public async Task<IActionResult> OnGet(string returnUrl = null)
         {
             await _signInManager.SignOutAsync();
             _logger.LogInformation("User logged out.");
