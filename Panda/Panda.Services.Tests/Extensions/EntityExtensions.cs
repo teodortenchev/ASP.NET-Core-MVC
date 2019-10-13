e3a238b5fb9f0ce0bc0490610c4be12bbff34d4c
Teodor Tenchev <teodortenchev@gmail.com>
2019-10-13 22:47:48 +0400

    Added extension for clearing in memory db sets

---
+++ Panda/Panda.Services.Tests/Extensions/EntityExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Panda.Services.Tests.Extensions
+{
+    public static class EntityExtensions
+    {
+        public static void Clear<T>(this DbSet<T> dbSet) where T : class
+        {
+            dbSet.RemoveRange(dbSet);
+        }
+    }
+}
